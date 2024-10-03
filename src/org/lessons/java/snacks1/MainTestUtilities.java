e9a79dc81c60c30cf1d94a0d20b0600a7ae92afd
nmorrone <n.morrone96@gmail.com>
2024-10-03 10:10:59 +0200

    student-constructor-finish

---
+++ src/org/lessons/java/snacks1/MainTestUtilities.java
@@ -0,0 +1,12 @@
+package org.lessons.java.snacks1;
+
+public class MainTestUtilities {
+	
+	public static void main (String [] args){
+		
+		Studente studentePrimo = new Studente();
+		
+		System.out.println(studentePrimo.info);
+	}
+
+}
--- src/org/lessons/java/snacks1/Studente.java
+++ src/org/lessons/java/snacks1/Studente.java
@@ -1,5 +1,33 @@
 package org.lessons.java.snacks1;
 
+import java.util.Scanner;
+
+//definisco classe studente
 public class Studente {
 
+	String name;
+	String surname;
+	int age;
+	String studentInfo;
+	String info;
+	
+	Scanner input = new Scanner(System.in);
+	
+	//definisco costruttore
+	Studente(){
+		name = input.next();
+		surname = input.next();
+		age = input.nextInt();
+		info = getStudentInfo();
+
+	}
+	
+	String getStudentInfo() {
+		String info = name + " " + surname + ", " + String.valueOf(age);
+		return info;
+	}
+	
 }
+
+
+
